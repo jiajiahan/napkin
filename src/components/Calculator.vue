7e3b4ea268d33c2b0402f6be039806732384ec4c
Jiajia Han <62268854+HTSports@users.noreply.github.com>
2020-04-08 17:07:13 -0700

    Add easy results

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,7 +2,10 @@
   <div>
     <input v-model="expression" v-on:keyup.enter="calc" />
     <p>
-    Result: {{result}}
+    Results: {{result}}
+    <table align="center">
+      <tr v-for="item in easyResults" v-bind:key="item">{{item}}</tr>
+    </table>
     </p>
   </div>
 </template>
@@ -10,16 +13,18 @@
 <script>
 import * as mexp from 'math-expression-evaluator'
 
+const replaceMap = {
+  'p': '0.01',
+  'h': '100',
+  'k': '1 000',
+  'w': '10 000',
+  'm': '1000 000',
+  'y': '100 000 000',
+  'b': '1 000 000 000'
+}
+
 function expand(expression) {
-  const replaceMap = {
-    'p': '0.01',
-    'h': '100',
-    'k': '1 000',
-    'w': '10 000',
-    'm': '1000 000',
-    'y': '100 000 000',
-    'b': '1 000 000 000'
-  }
+
   const units = Object.keys(replaceMap).join('')
   expression = expression.replace(new RegExp(`([0-9]+(\\.[0-9]*)?[\\s${units}]*)`, 'g'), '($1)')
   for (const key in replaceMap) {
@@ -29,12 +34,26 @@ function expand(expression) {
   return expression
 }
 
+function toEasyResults(result) {
+  var results = []
+  for (const key in replaceMap) {
+    const element = replaceMap[key]
+    const num = mexp.eval(`${result} / ${element}`).toPrecision(2)
+    const unit = key == 'p' ? '%' : key
+    if (num >= 1 && num < 100) {
+      results.push(`${num} ${unit}`)
+    }
+  }
+  return results
+}
+
 export default {
   name: 'Calculator',
   data: function() {
     return {
       expression: '',
-      result: ''
+      result: '',
+      easyResults: []
     }
   },
   props: {
@@ -43,6 +62,7 @@ export default {
   methods: {
     calc: function() {
       var result = mexp.eval(expand(this.$data.expression))
+      this.$data.easyResults = toEasyResults(result)
       if (result > 1000) {
         result = Math.round(result)
       } else if (result > 1 && !Number.isInteger(result)) {
